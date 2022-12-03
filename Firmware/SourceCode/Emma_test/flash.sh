477951c4919caf33552195017c04455a3fa59fe4
Forairaaaaa <applesyqd@outlook.com>
2022-12-03 19:09:48 +0800

    add some scripts for lazzy shit

--- Firmware/SourceCode/Emma_test/flash.sh
+++ Firmware/SourceCode/Emma_test/flash.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-# This is a Build-Flash-Monitor script for WSl
-# Firstly, use command < usbipd wsl attach -a -b [BUSID] > on PowerShell to auto attach the board
+### This is for lazzy shit like me to build -> flash - > monitor project
+### Use < usbipd wsl attach -a -b [BUSID] > on PowerShell to auto attach the board
 
-# Serial port
+
+# Configs
+IDF_PATH=$HOME/esp/esp-idf
 SERIAL_PORT=/dev/ttyACM0
 
-echo "Build and flash to ${SERIAL_PORT}..."
 
-# Get serial port permission
-sudo chmod 777 ${SERIAL_PORT}
- 
+# Help shit
+help() {
+    sed -rn 's/^### ?//;T;p' "$0"
+}
+
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+    help
+    exit 1
+fi
+
 # Get idf
-. $HOME/esp/esp-idf/export.sh
+. ${IDF_PATH}/export.sh
 
 # Build and flash and monitor
-# idf.py -p ${SERIAL_PORT} flash monitor
-idf.py -p ${SERIAL_PORT} flash -b 115200 monitor
+idf.py -p ${SERIAL_PORT} flash monitor
 
