e4beafa22fd5e5c3e1154ee2b952373ee854b773
Forairaaaaa <applesyqd@outlook.com>
2022-12-04 18:38:34 +0800

    add component lovyangfx

---
+++ Firmware/SourceCode/Emma_test/main/Emma_test.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Emma_test.cpp
+ * @author Forairaaaaa
+ * @brief App entrance
+ * @version 0.1
+ * @date 2022-12-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "Emma.h"
+
+Emma emma;
+
+extern "C" void app_main(void)
+{
+    emma.Init();
+
+    while (1) {
+        ;
+    }
+}
