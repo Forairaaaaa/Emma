39ebb92ded08cfc9d08d8390453851ac8442589b
Forairaaaaa <applesyqd@outlook.com>
2022-12-06 00:18:10 +0800

    remove some trash

--- Firmware/SourceCode/Emma_test/main/Emma_test.cpp
+++ Firmware/SourceCode/Emma_test/main/Emma_test.cpp
@@ -26,33 +26,6 @@ extern "C" void app_main(void)
 
 
     Emma.BuzzerTest();
-    
-
-    // led_strip_config_t led_gpio;
-    // led_gpio.max_leds = 1;
-    // led_gpio.strip_gpio_num = EMMA_PIN_RGB_LED;
-    // led_strip_rmt_config_t led_rmt;
-    // led_rmt.resolution_hz = 0;
-    // led_strip_handle_t rgb_led;
-
-    // led_strip_new_rmt_device(&led_gpio, &led_rmt, &rgb_led);
-
-
-    // while (1) {
-    //     led_strip_set_pixel(rgb_led, 0, 255, 0, 0);
-    //     led_strip_refresh(rgb_led);
-    //     printf("%s", emma.Cowsay("R", ANSI_RED).c_str());
-    //     EmmaDelay(1000);
-    //     led_strip_set_pixel(rgb_led, 0, 0, 255, 0);
-    //     led_strip_refresh(rgb_led);
-    //     printf("%s", emma.Cowsay("G", ANSI_GREEN).c_str());
-    //     EmmaDelay(1000);
-    //     led_strip_set_pixel(rgb_led, 0, 0, 0, 255);
-    //     led_strip_refresh(rgb_led);
-    //     printf("%s", emma.Cowsay("B", ANSI_BLUE).c_str());
-    //     EmmaDelay(1000);
-    // }
-
 
     while (1) {
         Emma.RgbLedShow(0xFF0000);
