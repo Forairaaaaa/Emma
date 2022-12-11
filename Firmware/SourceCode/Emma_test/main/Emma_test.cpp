a3ca9929fe987dc553a923ac241a44882ac09682
Forairaaaaa <applesyqd@outlook.com>
2022-12-11 23:46:35 +0800

    hah

--- Firmware/SourceCode/Emma_test/main/Emma_test.cpp
+++ Firmware/SourceCode/Emma_test/main/Emma_test.cpp
@@ -11,17 +11,8 @@
 #include "Emma.h"
 EMMA Emma;
 
-
-
 #include "Encoder.h"
 Encoder ec;
-
-
-
-
-
-
-static void LedTask(void* param);
 
 
 extern "C" void app_main(void)
@@ -29,50 +20,21 @@ extern "C" void app_main(void)
     Emma.Init();
 
     printf(Emma.Cowsay("Meow~~").c_str());
-    Emma.lcd.printf(Emma.Cowsay("Meow~~").c_str());
-
-    xTaskCreate(LedTask, "RGB-LED", 1024, NULL, 1, NULL);
-
+    Emma.Lcd.printf(Emma.Cowsay("Meow~~").c_str());
     Emma.BuzzerTest();
 
+    
 
-
-
-
-    ec.Init(EMMA_PIN_EC_A, EMMA_PIN_EC_B);
+    ec.Init(EMMA_PIN_EC_A, EMMA_PIN_EC_B, EMMA_PIN_EC_BTN);
     while (1) {
-        printf("%d\n", ec.GetDirection());
-        EmmaDelay(100);
+        cout << ec.Btn.pressed() << endl;
+        delay(100);
     }
 
 
-
-
-    
-
-
-    
-
     while (1) {
-        EmmaDelay(5000);
+        delay(5000);
     }
 }
 
 
-
-static void LedTask(void* param)
-{
-    uint8_t brightness = 0;
-    uint8_t color = 0;
-    while (1) {
-        Emma.RgbLedShow((brightness++) << color);
-        if (brightness == 70) {
-            brightness = 0;
-            color += 8;
-            if (color > 16)
-                color = 0;
-        }
-        EmmaDelay(40);
-    }
-    vTaskDelete(NULL);
-}
